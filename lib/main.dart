4b43d5626030b6adc0baa27302f18c4aff6c93a1
muridi010 <mohamudsharif017@gmail.com>
2020-01-26 15:20:29 +0300

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     ),
   );
