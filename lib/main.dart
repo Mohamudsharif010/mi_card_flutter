f46cd131cba8473036a980eeb1b307e4a027cee6
muridi010 <mohamudsharif017@gmail.com>
2020-01-27 21:16:55 +0300

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light,
+  ));
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
